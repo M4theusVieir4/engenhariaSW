6fdbd18a039a92f11f98a93cd763662e2d75cef3
M4theusVieir4 <vieiramatheus72@gmail.com>
2023-05-07 02:27:48 -0300

    Pegando através do get no firebase

--- engenhariaSW/src/main.js
+++ engenhariaSW/src/main.js
@@ -7,6 +7,23 @@ import App from './App.vue';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjCF7baoRDztDMhJC8d0x_j96hUL3uaU8",
+  authDomain: "engenhariasw-99bde.firebaseapp.com",
+  projectId: "engenhariasw-99bde",
+  storageBucket: "engenhariasw-99bde.appspot.com",
+  messagingSenderId: "189158024420",
+  appId: "1:189158024420:web:9ad00f52c3ffa707b4b8f1",
+  measurementId: "G-FQ571XVX75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -15,4 +32,6 @@ const router = createRouter({
   ],
 });
 
+
 createApp(App).use(router).mount('#app');
+export{ db };
--- engenhariaSW/src/views/About.vue
+++ engenhariaSW/src/views/About.vue
@@ -4,3 +4,48 @@
         <router-link to="/">Go to Home</router-link>
     </div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+import {
+ref as firebaseRef,
+set as firebaseSet,
+get as firebaseGet,
+onDisconnect,
+onValue,
+update,
+onChildRemoved,
+query,
+child,
+get,
+} from 'firebase/database';
+import { db } from '../main.js';
+
+function setarHost(){
+    const host = firebaseRef(db, `host/chave/`);
+    firebaseSet(host, 'playerId');
+}
+async function getHost() {
+    const host = firebaseRef(db, `host/chave/`);
+    //let res = (await firebaseGet(host)).val()
+    //console.log(res)
+    let res = await firebaseGet(host)
+    console.log(res.val())
+}
+
+export default {
+    name: 'About',
+    components: {},
+    methods: {
+        
+
+    },
+    setup() {
+        //setarHost()
+        getHost()
+    },
+    data() {},
+    beforeMount() {}
+}
+</script>
