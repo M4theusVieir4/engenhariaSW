3045d4f8631be3ed99214849801f3b544d0df7e1
M4theusVieir4 <vieiramatheus72@gmail.com>
2023-05-13 19:28:18 -0300

    #criando a página do atendimento

--- engenhariaSW/src/App.vue
+++ engenhariaSW/src/App.vue
@@ -1,12 +1,4 @@
 <template>
-  <img
-    alt="Vue logo"
-    class="logo"
-    src="./assets/logo.svg"
-    width="125"
-    height="125"
-  />
-
   <router-view></router-view>
 </template>
 
--- engenhariaSW/src/views/Home.vue
+++ engenhariaSW/src/views/Home.vue
@@ -2,5 +2,7 @@
     <div class="home">
         <h1>Home</h1>
         <router-link to="/about">Go to About</router-link>
+        <br>
+        <router-link to="/colaboradores">Go to Colaboradores</router-link>
     </div>
 </template>
